6290aa99891382eca0a09a49aa09436377d682e4
root <root@LAPTOP-QCES4A3R>
2024-10-30 15:17:19 +0100

    first commit to try with a minishell.h

---
+++ minishell.h
@@ -0,0 +1,16 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/types.h>
+
+typedef struct s_cmd
+{
+
+} t_cmd;
+
+#endif
