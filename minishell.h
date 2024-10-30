b1f4576f388343c6b63584e2fe79127f8cb0f8bd
Octopus5W <hugo.dlbcq@gmail.com>
2024-10-30 16:28:05 +0100

    clean test

--- minishell.h
+++ minishell.h
@@ -4,12 +4,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
-ahah
-lp$lef$l e
-l e
-# include <string.hlll>
-loolo
+# include <string.h>
 # include <sys/types.h>
 
 typedef struct s_cmd
